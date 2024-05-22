eb49e5637d994178eb622f6b3b2a3df1b6a00aef
William Todd <163104612+phatnip101@users.noreply.github.com>
2024-05-22 16:51:14 +1000

    Added Will's Scene + Extras
    Added other skin construction Pete and jumping animation that needs work.

--- IGB101 Group Assignment 3/Assets/Third Person Controller-1/PlayerMovement.cs
+++ IGB101 Group Assignment 3/Assets/Third Person Controller-1/PlayerMovement.cs
@@ -8,9 +8,19 @@ public class PlayerMovement : MonoBehaviour{
 
     public float rotSpeed = 10;
 
+    public float speed;
+    public float rotationSpeed;
+    public float jumpSpeed;
+
+    private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset;
+
     // Start is called before the first frame update
-    void Start(){
-        
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
     // Update is called once per frame
@@ -27,6 +37,45 @@ public class PlayerMovement : MonoBehaviour{
         Moving();
 
         Jumping();
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        
+
+        if (characterController.isGrounded)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed;
+                anim.SetBool("Jumping", true);
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+            anim.SetBool("Jumping", false);
+        }
+
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
 
     }
 
@@ -83,13 +132,18 @@ public class PlayerMovement : MonoBehaviour{
     }
     private void Jumping()
     {
-        if (Input.GetKeyDown("space"))
+
+        if (GetComponent<CharacterController>().isGrounded)
         {
-            anim.SetBool("Jumping", true);
-        }
-        else if (Input.GetKeyUp("space"))
-        {
-            anim.SetBool("Jumping", false);
-        }
+            if (Input.GetKeyDown("space"))
+            {
+                anim.SetBool("Jumping", true);
+            }
+            else if (Input.GetKeyUp("space"))
+            {
+                anim.SetBool("Jumping", false);
+            }
+  
     }
+}
 }
