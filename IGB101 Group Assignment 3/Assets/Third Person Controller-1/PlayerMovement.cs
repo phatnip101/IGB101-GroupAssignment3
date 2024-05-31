1a433e64c914015362ea0b2322bcacdbe4a90fc3
William Todd <163104612+phatnip101@users.noreply.github.com>
2024-05-31 17:49:11 +1000

    Will's Scene Complete

--- IGB101 Group Assignment 3/Assets/Third Person Controller-1/PlayerMovement.cs
+++ IGB101 Group Assignment 3/Assets/Third Person Controller-1/PlayerMovement.cs
@@ -49,18 +49,18 @@ public class PlayerMovement : MonoBehaviour{
         if (characterController.isGrounded)
         {
             characterController.stepOffset = originalStepOffset;
-            ySpeed = -0.5f;
+            //ySpeed = -0.5f;
 
             if (Input.GetButtonDown("Jump"))
             {
                 ySpeed = jumpSpeed;
-                anim.SetBool("Jumping", true);
+                //anim.SetBool("Jumping", true);
             }
         }
         else
         {
             characterController.stepOffset = 0;
-            anim.SetBool("Jumping", false);
+            //anim.SetBool("Jumping", false);
         }
 
         Vector3 velocity = movementDirection * magnitude;
@@ -77,28 +77,41 @@ public class PlayerMovement : MonoBehaviour{
 
     }
 
-    private void ForwardMovement(){
-        if(Input.GetKey("w")){
+    private void ForwardMovement()
+    {
+        if (Input.GetKey("w"))
+        {
             anim.SetBool("Walking", true);
-            if (Input.GetKey(KeyCode.LeftShift)){
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
                 anim.SetBool("Running", true);
-            } else{
+            }
+            else
+            {
                 anim.SetBool("Running", false);
             }
-        } else if (Input.GetKeyUp("w")) {
+        }
+        else if (Input.GetKeyUp("w"))
+        {
             anim.SetBool("Walking", false);
             anim.SetBool("Running", false);
         }
     }
 
-    private void Turning(){
-        if (Input.GetKey("a")) {
+    private void Turning()
+    {
+        if (Input.GetKey("a"))
+        {
             transform.Rotate(0, -rotSpeed * 15 * Time.deltaTime, 0, Space.World);
             anim.SetBool("Turn Left", true);
-        } else if (Input.GetKey("d")) {
+        }
+        else if (Input.GetKey("d"))
+        {
             transform.Rotate(0, rotSpeed * 15 * Time.deltaTime, 0, Space.World);
             anim.SetBool("Turn Right", true);
-        } else {
+        }
+        else
+        {
             anim.SetBool("Turn Left", false);
             anim.SetBool("Turn Right", false);
         }
